6be78ec7919144d9ad3182c97a5ff1fd63af830e
Eduarda Silva <52262385+eduardassilva@users.noreply.github.com>
2020-12-09 21:43:23 -0300

    Add files via upload

--- srcRestaurant/index.php
+++ srcRestaurant/index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" href="styles/styleLogin.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<script src="scripts/login.js"></script>
+		<!--<script src="scripts/login.js"></script>-->
         <title>Login</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -28,11 +28,11 @@
                     <input type="text" id="login" name="login" placeholder="Username" autocomplete="off">
                     <input type="password" id="password" name="password" placeholder="Senha">
                     <br><br>
-                    <button id="submit" class="btn btn-lg">Entrar</button>
+                    <button id="submit" class="btn btn-lg" onclick="window.location = 'pageRestaurant.php'">Entrar</button>
                     <br><br>
                 </form>
                 <div id="formFooter">
-                    <a type="button" class="underlineHover" onclick="window.location = 'pageRestaurant.php' <!-- data-toggle="modal" data-target="#myModalLogin"-->>Esqueceu a senha?</a>
+                    <a type="button" class="underlineHover"  <!-- data-toggle="modal" data-target="#myModalLogin"-->>Esqueceu a senha?</a>
                 </div>
                 <div id="myModalLogin" class="modal fade" role="dialog">
 					<div class="modal-dialog">
