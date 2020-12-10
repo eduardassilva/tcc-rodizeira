9fe1df06123ccedf8320c0d902ce2383316403a5
Eduarda Silva <52262385+eduardassilva@users.noreply.github.com>
2020-12-09 21:24:03 -0300

    Add files via upload

--- srcRestaurant/scripts/login.js
+++ srcRestaurant/scripts/login.js
@@ -1,16 +1,16 @@
-var loginRequired = 'admin';
+/*var loginRequired = 'admin';
 var passwordRequired = '123';
 
 var loginInput;
-var passwordInput;
+var passwordInput;*/
 
 function Login(){
-	
+	/*
 	loginInput = document.getElementById('login');
-	passwordInput = document.getElementById('password');
+	passwordInput = document.getElementById('password');*/
 	
 //	if(loginInput.value == loginRequired && passwordInput.value == passwordRequired)
-		window.location = "pageRestaurant.html";
+		window.location = "pageRestaurant.php";
 	/*else{
 		alert('Login ou Senha estão incorretos!');
 		passwordInput.value = '';
