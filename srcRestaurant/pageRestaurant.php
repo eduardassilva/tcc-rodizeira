99b9c2ee238f5a590aa671f09d5da64f952696f0
Eduarda Silva <52262385+eduardassilva@users.noreply.github.com>
2020-12-12 23:44:08 -0300

    Add files via upload

--- srcRestaurant/pageRestaurant.php
+++ srcRestaurant/pageRestaurant.php
@@ -10,6 +10,8 @@
 		<link rel="stylesheet" href="styles/cardapio.css"/>
 		<link rel="stylesheet" href="styles/perfil.css"/>
 		<link rel="stylesheet" href="styles/rodizio.css"/>
+        <link rel="stylesheet" href="styles/pedidos.css"/>
+        <link rel="stylesheet" href="styles/inicio.css"/>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 		<script src="scripts/main.js"></script><script src="scripts/inicio.js"></script><script src="scripts/pedidos.js"></script>
 		<script src="scripts/cardapio.js"></script><script src="scripts/perfil.js"></script><script src="scripts/rodizio.js"></script><script src="scripts/etiquetas.js"></script>
@@ -21,8 +23,8 @@
     <body background="img/bg1.png" onload="StartPage()">
 		<div id=nav>
 			<div class="navBar" id="navBarID">
-				<img id="logoRestaurant" src="img/llLogo.png" class="logoImage" alt="Missing Logo" style="color:white"></img>
-				<p class="navHeader" id="restaurantName">Luda Lanches</p>
+				<img id="logoRestaurant" src="img/bbqLogo.png" class="logoImage" alt="Missing Logo" style="color:white"></img>
+				<p class="navHeader" id="restaurantName">Churrascaria Gado Demais</p>
 				<section class="navDiv">
 					<a class="navButton" onclick="OpenTab(event, 'navInicio')" id="defaultOpen">Início</a>
 					<a class="navButton" onclick="OpenTab(event, 'navPedidos')">Pedidos</a>
@@ -42,17 +44,64 @@
 			<div id="navInicio" class="tabContent">
 				<h3>Início</h3>
 				<div class="line"></div>
+                <div id="divMaisPedidos">
+                    <div id="maisPedidosSemana">
+                        <h4>Mais pedidos da Semana:</h4>
+                    </div>
+                    <div id="maisPedidosSemestre">
+                        <h4>Mais pedidos do Semestre:</h4>
+                    </div>
+                    <div id="maisPedidosMes">
+                        <h4>Mais pedidos do Mês:</h4>
+                    </div>
+                </div>
 				<br><br><br>
-
             </div>
 			
+            <div id="navPedidos" class="tabContent">
+				<h3>Pedidos</h3>
+				<div class="line"></div>
+                <div id="pedidosDiv">
+					<!--<div class="divPedido">
+						<div class="divTitle">
+							<p class="pedidoName">Pedido #1</p>
+							<div class="pedidoDeleteDiv">
+								<a href="#">DELETAR</a>
+							</div>
+						</div>
+						<div class="line"></div>
+						<div class="divItemsPedido">
+							<p class="itemsPedido">- Pizza 1</p>
+						</div>
+					</div>-->
+				</div>
+            </div>
+            
             <div id="navRodizio" class="tabContent">
 				<h3 class="tabHeader">Rodízio</h3>
 				<button type="button" class="btn btn-success addCardapio" data-toggle="modal" data-target="#modalRodizio">
 					Adicionar
 				</button>
 				<div class="line"></div>
-				<div id="rodiziosDiv"></div>
+                <br>
+				<div id="rodiziosDiv">
+					<!--<div class="rodizio">
+						<div class="rodizioPrice">11.11</div>
+						<div class="rodizioName">hdhsfds</div>
+						<div class="rodizioDeleteDiv">
+							<a href="#">DELETAR</a>
+						</div>
+						<div class="rodizioTags">
+							<p># Exemplo 5</p> 
+							<p># Exemplo 6</p>
+						</div>
+						
+						<div class="rodizioDesc">dhhsdhfksdjhfksdhkdshf</div>
+					</div>-->
+					
+				</div>
+				
+
                 
                 <!-- Modal -->
 				<div id="modalRodizio" class="modal fade" role="dialog">
@@ -75,7 +124,40 @@
 								</label><br><br>
 								<label>Selecione as tags: </label>
 								<br>
-								<div id="divTagRodizio" class="divTagRodizio"></div>
+								<div id="divTagRodizio" class="divTagRodizio">
+									<!--<label for="tagRodizio1">
+										<input type="checkbox" id="tagRodizio1" class="tagCardapio" autocomplete="off">
+										Exemplo 1   
+									</label>
+									<label for="tagRodizio2">
+										<input type="checkbox" id="tagRodizio2" class="tagCardapio" autocomplete="off">
+										Exemplo 2   
+									</label> 
+									<label for="tagRodizio3">
+										<input type="checkbox" id="tagRodizio3" class="tagCardapio" autocomplete="off">
+										Exemplo 3   
+									</label>
+									<label for="tagRodizio4">
+										<input type="checkbox" id="tagRodizio4" class="tagCardapio" autocomplete="off">
+										Exemplo 4   
+									</label> 
+									<label for="tagRodizio5">
+										<input type="checkbox" id="tagRodizio5" class="tagCardapio" autocomplete="off">
+										Exemplo 5   
+									</label>
+									<label for="tagRodizio6">
+										<input type="checkbox" id="tagRodizio6" class="tagCardapio" autocomplete="off">
+										Exemplo 6   
+									</label> 
+									<label for="tagRodizio7">
+										<input type="checkbox" id="tagRodizio7" class="tagCardapio" autocomplete="off">
+										Exemplo 7   
+									</label>
+									<label for="tagRodizio8">
+										<input type="checkbox" id="tagRodizio8" class="tagCardapio" autocomplete="off">
+										Exemplo 8   
+									</label> -->
+								</div>
 								
 							</div>
 							<div class="modal-footer">
@@ -87,25 +169,45 @@
 				</div>
                 
 			</div>			
-			
-			<div id="navPedidos" class="tabContent">
-				<h3>Pedidos</h3>
-				<div class="line"></div>
-                
-            </div>
-              
+ 
             <div id="navCardapio" class="tabContent">		
 				<div id="divCardapio"> 
 					<h3 class="cardapio activeCardapio">Cardápio</h3>
-					<h3 class="cardapio" id="cardapioTag" onclick="OpenSubTab(event, 'navTags')">Tags</h3>
+					<h3 class="cardapio" id="cardapioTag" onclick="OpenSubTab(event, 'navTags')">Etiquetas</h3>
 					<button type="button" class="btn btn-success addCardapio" data-toggle="modal" data-target="#myModal">
 						Adicionar	
 					</button>
 				</div>
 				
 				<div class="line"></div>
-				
-				<div id="itemsCardapio"></div>
+				<br>
+				<div id="itemsCardapio">
+					<!--<div class="cardapioItemDiv">
+						<div class="cardapioImg"></div>
+						<div class="cardapioName">
+							<p>dsfsdf</p>
+						</div>
+						<div class="cardapioDeleteDiv">
+							<a href="#">DELETAR</a>
+						</div>
+						<div class="cardapioDescDiv">
+							<p>hdsugdsgfjcvbkuidsgfjsdhfbvjvcdsvfksdhbdnjsvfikufjhd</p>
+							<p>hdsugdsgfjcvbkuidsgfjsdhfbvjvcdsvfksdhbdnjsvfikufjhd</p>
+							<p>hdsugdsgfjcvbkuidsgfjsdhfbvjvcdsvfksdhbdnjsvfikufjhd</p>
+							<p>hdsugdsgfjcvbkuidsgfjsdhfbvjvcdsvfksdhbdnjsvfikufjhd</p>
+						</div>
+						<div class="cardapioTag">
+							<label for="tagCardapio5">
+								#Exemplo 5, 
+							</label>
+							<label for="tagCardapio6">
+								#Exemplo 6   
+							</label> 
+						</div>
+						
+
+					</div>-->
+				</div>
 
 				<!-- Modal -->
 				<div id="myModal" class="modal fade" role="dialog">
@@ -131,6 +233,38 @@
 								<label>Selecione as etiquetas: </label>
 								<br>
 								<div id="divTagCardapio" class="divTagCardapio">
+									<!--<label for="tagCardapio1">
+										<input type="checkbox" id="tagCardapio1" class="tagCardapio">
+										Exemplo 1   
+									</label>
+									<label for="tagCardapio2">
+										<input type="checkbox" id="tagCardapio2" class="tagCardapio">
+										Exemplo 2   
+									</label> 
+									<label for="tagCardapio3">
+										<input type="checkbox" id="tagCardapio3" class="tagCardapio">
+										Exemplo 3   
+									</label>
+									<label for="tagCardapio4">
+										<input type="checkbox" id="tagCardapio4" class="tagCardapio">
+										Exemplo 4   
+									</label> 
+									<label for="tagCardapio5">
+										<input type="checkbox" id="tagCardapio5" class="tagCardapio">
+										Exemplo 5  
+									</label>
+									<label for="tagCardapio6">
+										<input type="checkbox" id="tagCardapio6" class="tagCardapio">
+										Exemplo 6   
+									</label> 
+									<label for="tagCardapio7">
+										<input type="checkbox" id="tagCardapio7" class="tagCardapio">
+										Exemplo 7   
+									</label>
+									<label for="tagCardapio8">
+										<input type="checkbox" id="tagCardapio8" class="tagCardapio">
+										Exemplo 8   
+									</label>-->
 								</div>
 								
 							</div>
@@ -147,7 +281,7 @@
 			<div id="navTags" class="tabContent">
 				<div id="divCardapio"> 
 					<h3 class="cardapio" onclick="OpenSubTab(event, 'navCardapio')">Cardápio</h3>
-					<h3 class="cardapio activeCardapio" id="cardapioTag">Tags</h3>
+					<h3 class="cardapio activeCardapio" id="cardapioTag">Etiquetas</h3>
 					<button type="button" class="btn btn-success addTag" data-toggle="modal" data-target="#myModalTags">
 						Adicionar	
 					</button>
@@ -159,11 +293,11 @@
 						<div class="modal-content">
 							<div class="modal-header">
 								<button type="button" class="close" data-dismiss="modal">&times;</button>
-								<h4 class="modal-title">Adicionar nova Tag</h4>
+								<h4 class="modal-title">Adicionar nova Etiqueta</h4>
 							</div>
 							<div class="modal-body">
 								<label>
-									Nome da Tag <br><input type="text" id="nomeTag" maxlength="40" autocomplete="off"/>
+									Nome da Etiqueta <br><input type="text" id="nomeTag" maxlength="40" autocomplete="off"/>
 								</label> 
 							</div>
 							<div class="modal-footer">
@@ -174,7 +308,7 @@
 					</div>
 				</div>
 				<div id="divTagsID" class="divTag">
-					<h3>Tags Adicionadas</h3>
+					<h3>Etiquetas Adicionadas</h3>
 					<br><br>
 				</div>
 
@@ -193,7 +327,7 @@
 						<h3>Informações gerais</h3>
 						<br><br>
 						<label>
-							<p>Nome do Restaurante</p><input type="text" id="restaurantNameInput" placeholder="Luda Lanches" maxlength="40" size="40" autocomplete="off"/>
+							<p>Nome do Restaurante</p><input type="text" id="restaurantNameInput" maxlength="40" size="40" autocomplete="off"/>
 						</label><br><br>
 						<label>
 							<p>Logo do Restaurante:</p><input type="file" id="logoImageInput" onChange="readURLLogo(this, true)" autocomplete="off"/>
@@ -206,11 +340,11 @@
 						<h3>Informações de contato</h3>
 						<br><br>
 						<label>
-							<p>Telefone</p><input type="tel" id="telefoneInput" placeholder="51 993992523" maxlength="40" size="40" onChange="TelefoneValidation(this)" autocomplete="off"/>
+							<p>Telefone</p><input type="tel" id="telefoneInput" maxlength="40" size="40" onChange="TelefoneValidation(this)" autocomplete="off"/>
 							<p class="observationInput">* Insira somente números no formato XX XXXXXXXX</p>
 						</label><br><br>
 						<label>
-							<p>E-Mail</p><input type="text" id="emailInput" placeholder="eduarda.ssilva@hotmail.com" maxlength="40" size="40" onChange="EmailValidation(this)" autocomplete="off"/>
+							<p>E-Mail</p><input type="text" id="emailInput" maxlength="40" size="40" onChange="EmailValidation(this)" autocomplete="off"/>
 						</label>
 						<br><br>
 					</div>
@@ -218,17 +352,17 @@
 						<h3>Localização</h3>
 						<br><br>
 						<label>
-							<p>CEP</p><input type="text" id="cepInput" placeholder="90840-440" maxlength="40" size="40" onChange="CEPValidation(this)" autocomplete="off"/>
+							<p>CEP</p><input type="text" id="cepInput" maxlength="40" size="40" onChange="CEPValidation(this)" autocomplete="off"/>
 							<p class="observationInput">* Insira somente números no formato XXXXXXXX</p>
 						</label><br><br>
 						<label>
-							<p>Estado</p><input type="text" id="estadoInput" placeholder="Rio Grande do Sul" maxlength="40" size="40" autocomplete="off"/>
+							<p>Estado</p><input type="text" id="estadoInput" maxlength="40" size="40" autocomplete="off"/>
 						</label><br><br>
 						<label>
-							<p>Cidade</p><input type="text" id="cidadeInput" placeholder="Porto Alegre" maxlength="40" size="40" autocomplete="off"/>
+							<p>Cidade</p><input type="text" id="cidadeInput" maxlength="40" size="40" autocomplete="off"/>
 						</label><br><br>
 						<label>
-							<p>Endereço</p><input type="text" id="enderecoInput" placeholder="Orfanotrofio, 555" maxlength="40" size="40" autocomplete="off"/>
+							<p>Endereço</p><input type="text" id="enderecoInput" maxlength="40" size="40" autocomplete="off"/>
 						</label><br><br>
 					</div>
 				</div>
@@ -274,24 +408,57 @@
 		<footer>
 			<p>Rodizeira © Copyright 2020. Todos os direitos reservados</p>
 		</footer>
-        <!--?php
+        <?php/*
             include 'C:\wamp64\www\TCC-Rodizeira\srcRestaurant\php\startRestaurant.php';
+            fillInicio();
             fillRestaurantInfo();
             fillCardapio();
             fillRodizio();
             fillEtiquetas();
-		?-->
-		<script> 
-			CreateDivItemCardapio('Calabresa', 'Mussarela, calabresa e cebola, oregano.', '#Carne~');
-			CreateDivItemCardapio('4 Queijos', 'Mussarela, requeijão, gorgonzola, oregano e parmesão ralado.', '#Frios~');
-			CreateDivItemCardapio('Alho e Óleo', 'Alho e óleo, salsa e parmesão', '#Legumes~');
-			CreateDivItemCardapio('Tentação', 'Chocolate preto e morangos', '#Doces~');
-			CreateTag('Carne');
-			CreateTag('Frios');
-			CreateTag('Legumes');
-			CreateTag('Doces');
-			CreateDivRodizio('Completo', '32.90', 'Rodizio completo', '#Carne~#Frios~#Legumes~#Doces~');
-			CreateDivRodizio('Vegetariano', '25.90', 'Rodizio vegetariano', '#Frios~#Legumes~#Doces~');
-		</script>
+            fillPedidos();*/
+        ?>
+        <script>
+            SetPerfilTab('Churrascaria Gado Demais', 'img/bbqLogo.png', '(51) 3387-3733', 'gadodemais@contato.com.br', '90470-340', 'Rio Grande do Sul', 'Porto Alegre', 'Avenida Soledade, Número 374');
+        
+            CreateTag('Acompanhamentos');CreateTag('Galeto');CreateTag('Classico');CreateTag('Completo');
+            
+            CreateDivItemCardapio('Peito de Frango', '', '#Galeto~');
+            CreateDivItemCardapio('Coxa de Frango', '', '#Galeto~');
+            CreateDivItemCardapio('Sobrecoxa de Frango', '', '#Galeto~');
+            CreateDivItemCardapio('Coração de Frango', '', '#Galeto~');
+            CreateDivItemCardapio('Asa de Frango', '', '#Galeto~');
+            CreateDivItemCardapio('Sassami Grelhado', '', '#Galeto~');
+            CreateDivItemCardapio('Costela Suína', '', '#Galeto~');
+            CreateDivItemCardapio('Salsichão', '', '#Galeto~');
+            CreateDivItemCardapio('Filé Suíno com Bacon', '', '#Galeto~');
+            CreateDivItemCardapio('Filé de Costela', '', '#Galeto~');
+            CreateDivItemCardapio('Costelinha Suína com Molho Barbecue', '', '#Classico~');
+            CreateDivItemCardapio('Filé de Costela', '', '#Classico~');
+            CreateDivItemCardapio('Costela', '', '#Classico~');
+            CreateDivItemCardapio('Costelão 12 Horas', '', '#Classico~');
+            CreateDivItemCardapio('Cupim', '', '#Classico~');
+            CreateDivItemCardapio('Maminha', '', '#Classico~');
+            CreateDivItemCardapio('Vazio', '', '#Classico~');
+            CreateDivItemCardapio('Picanha', '', '#Completo~');
+            CreateDivItemCardapio('Alcatra', '', '#Completo~');
+            CreateDivItemCardapio('Filé Mignon', '', '#Completo~');
+            CreateDivItemCardapio('Contrafilé', '', '#Completo~');
+            CreateDivItemCardapio('Fraldinha', '', '#Completo~');
+            CreateDivItemCardapio('Linguiça Calabresa', '', '#Completo~');
+            CreateDivItemCardapio('Pão de Alho', 'Pão francês com alho e maionese da casa', '#Acompanhamentos~');
+            CreateDivItemCardapio('Queijo Coalho', '', '#Acompanhamentos~');
+            CreateDivItemCardapio('Polenta Frita', '', '#Acompanhamentos~');
+            CreateDivItemCardapio('Batata Frita', '', '#Acompanhamentos~');
+            
+            CreateDivRodizio('Rodízio de Galeto', '37.90', 'Sequência inclui TODOS os cortes de frango da casa mais TRÊS cortes suínos [costela suína, salsichão e filé suíno com bacon], mais UM corte bovinho [filé de costela], além de acompanhamentos.', '#Galeto~#Acompanhamentos~');
+            CreateDivRodizio('Rodízio Clássico', '57.90', 'Sequência inclui TODOS os cortes de frango da casa mais QUATRO cortes suínos [costela suína, salsichão, filé suíno com bacon e costelinha suína com molho barbecue], mais SEIS cortes bovinhos [filé de costela, costela, costelão 12 horas, cupim, maminha e vazio], além de acompanhamentos.', '#Galeto~#Classico~#Acompanhamentos~');
+            CreateDivRodizio('Rodízio Completo', '79.90', 'Sequência de Carnes Nobres incluindo TODOS os cortes da casa, entre eles frango , suíno, bovino e cordeiro, além de acompanhamentos.', '#Galeto~#Classico~#Completo~#Acompanhamentos~');
+            
+            CreatePedido('Costelinha Suína com Molho Barbecue');
+            CreatePedido('Linguiça Calabresa');
+            CreatePedido('Pão de Alho');
+            CreatePedido('Queijo Coalho');
+            CreatePedido('Polenta Frita');
+        </script>
     </body>
 </html>
